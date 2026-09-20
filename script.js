ce22454cb3f61b3b805fbd2d5a39379810ea62f4
alexxturner3 <109403833+alexxturner3@users.noreply.github.com>
2026-09-20 20:18:57 +1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// Live datetime
+function updateTime() {
+  const now = new Date();
+ const day = now.toLocaleDateString('en-NZ', {
+    weekday: 'short'
+  });
+  const time = now.toLocaleTimeString('en-NZ', {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: true
+  });
+
+ 
+
+  const date = now.getDate();
+  const suffix =
+    date % 10 === 1 && date !== 11 ? 'st' :
+    date % 10 === 2 && date !== 12 ? 'nd' :
+    date % 10 === 3 && date !== 13 ? 'rd' : 'th';
+
+  document.getElementById("datetime").innerHTML =
+    `NZT ${time}, ${day} ${date}${suffix}`;
+}
+
+setInterval(updateTime, 1000);
+updateTime();
